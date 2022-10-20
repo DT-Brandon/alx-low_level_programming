6449f953da561dc6144f2817181fb9493eaa4d7a
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2022-10-20 15:04:05 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,11 @@
 #include "holberton.h"
 
-
-
 /**
-
  * _isupper - checks for uppercase character
-
  * @c: the character to be checked
-
  * Return: 1 if c is uppercase, 0 otherwise
-
  */
-
 int _isupper(int c)
-
 {
-
-README.md return (c >= A && c <= Z);
-
+	return (c >= 'A' && c <= 'Z');
 }
