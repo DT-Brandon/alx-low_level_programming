70e60617b66ae8f1c78650920434a1e061e82b4b
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-03-24 04:57:08 -0700

    task 5 done

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,12 +6,17 @@ def island_perimeter(grid):
     """ calculates the perimeter ofthe island grid """
 
     perimeter = 0
-    num_of_land = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                num_of_land += 1
-
-    perimeter = 2*num_of_land + 2
+                if i = 0 or grid[i-1][j] = 0
+                    perimeter += 1
+                if j = 0 or grid[i][j-1] = 0
+                    perimeter += 1
+                if j = len(grid[i]) - 1 or grid[i][j+1] = 0
+                    perimeter += 1
+                if i = len(grid) - 1 or grid[i+1][j] = 0
+                    perimeter += 1
 
     return perimeter            
